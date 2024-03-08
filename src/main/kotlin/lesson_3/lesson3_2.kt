b31e7209d3adda3991460fe823e2958286b90dd1
Afana <afanasevlad@mail.ru>
2024-03-08 21:21:46 +0300

    KS-3-2 create pac3

---
+++ src/main/kotlin/lesson_3/lesson3_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val surname1 = "Андреева"
+    val surname2 = "Сидорова"
+    val name = "Татьяна"
+    val fathersName = "Сергеевна"
+    val age1 = "20 лет"
+    val age2 = "22 года"
+
+    println("$surname1 $name $fathersName $age1")
+    println("$surname2 $name $fathersName $age2")
+
+}
