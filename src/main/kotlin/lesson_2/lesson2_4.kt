eed061ca88c31a0e165329ac050241f285d5aa02
Afana <afanasevlad@mail.ru>
2024-03-08 20:32:16 +0300

    KS-2-4 create smth

---
+++ src/main/kotlin/lesson_2/lesson2_4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val cristalOre: Int = 7
+    val ironOre: Int = 11
+
+    val bafCristalOre = cristalOre * 0.2
+    println("Добыто ${bafCristalOre.toInt()} бонусной кристалической руды")
+    val bafIronOre = ironOre * 0.2
+    println("Добыто ${bafIronOre.toInt()} бонусной железной руды")
+
+}
