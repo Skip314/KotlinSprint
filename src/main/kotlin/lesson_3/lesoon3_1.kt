f93e4e0814c14339a1fb3ee65f5c83cc1b9cfd20
Afana <afanasevlad@mail.ru>
2024-03-08 21:12:23 +0300

    KS-3-1 create pac3

---
+++ src/main/kotlin/lesson_3/lesoon3_1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val name = "Владислав"
+    val timeDay = "Добрый день,"
+    val timeNight = "Доброй ночи,"
+
+    var time = timeDay
+
+    println("$time $name")
+
+    time = timeNight
+
+    println("$time $name")
+
+}
