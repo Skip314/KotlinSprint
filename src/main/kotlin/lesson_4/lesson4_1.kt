800b7a0c22cc8fec50f040d254a7a107f2109c3c
Afana <afanasevlad@mail.ru>
2024-03-10 12:50:08 +0300

    KS-4-1 create pac3

---
+++ src/main/kotlin/lesson_4/lesson4_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val table = 13
+    val reserveTableToday = 13
+    val reserveTableTomorrow = 13 - 4
+
+    println("Доступность столиков на сегодня: ${reserveTableToday < table}")
+    println("Доступность столиков на завтра: ${reserveTableTomorrow < table}")
+
+
+
+}
