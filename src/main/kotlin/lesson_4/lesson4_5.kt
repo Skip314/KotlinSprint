0eee6bafc608121c54de002235dad58d139c9ac7
mac <afanasevlad@hotmail.com>
2024-03-12 10:10:11 +0300

    KS-4-5 readLine -> readln

--- src/main/kotlin/lesson_4/lesson4_5.kt
+++ src/main/kotlin/lesson_4/lesson4_5.kt
@@ -3,20 +3,20 @@ package org.example.lesson_4
 fun main() {
 
     println("Укажите поврежден ли корабль")
-    val bortCrash = readLine()
+    val bortCrash = readln()
 
     println("Введите колличество экипажа")
-    val worker = readLine()
+    val worker = readln()
 
     println("Укажите количество ящиков провизии")
-    val cargo = readLine()
+    val cargo = readln()
 
     println("Укажите погоду")
-    val weather = readLine()
+    val weather = readln()
 
 
-    val permission = (bortCrash.toBoolean() == false && worker!!.toInt() in 55 .. 70 && cargo!!.toInt() >= 50) ||
-            (bortCrash.toBoolean() == true && worker!!.toInt() == 70 && weather.toBoolean() == true && cargo!!.toInt() >= 50)
+    val permission = (bortCrash.toBoolean() == false && worker.toInt() in 55 .. 70 && cargo.toInt() >= 50) ||
+            (bortCrash.toBoolean() == true && worker.toInt() == 70 && weather.toBoolean() == true && cargo.toInt() >= 50)
 
-    println("Корабль может отправляться в плавание $permission")
+    println("Корабль может отправляться в плавание :$permission")
 }
