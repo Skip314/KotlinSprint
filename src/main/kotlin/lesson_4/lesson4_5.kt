d438c30a51a78da082045b3268f67b49a8e2e4c4
mac <afanasevlad@hotmail.com>
2024-03-12 10:58:58 +0300

    KS-4-5 add const

--- src/main/kotlin/lesson_4/lesson4_5.kt
+++ src/main/kotlin/lesson_4/lesson4_5.kt
@@ -2,8 +2,8 @@ package org.example.lesson_4
 
 fun main() {
 
-    println("Укажите поврежден ли корабль")
-    val bortCrash = readln()
+    println("Укажите поврежден ли корабль (true/false)")
+    val isDamaged = readln()
 
     println("Введите колличество экипажа")
     val worker = readln()
@@ -11,12 +11,16 @@ fun main() {
     println("Укажите количество ящиков провизии")
     val cargo = readln()
 
-    println("Укажите погоду")
-    val weather = readln()
+    println("Укажите погоду (true/false)")
+    val isWeatherGood = readln()
+
+    val rangeBoardWorker = 55 .. 70
+    val perfectQuantityWorker = 70
 
 
-    val permission = (bortCrash.toBoolean() == false && worker.toInt() in 55 .. 70 && cargo.toInt() >= 50) ||
-            (worker.toInt() == 70 && weather.toBoolean() == true && cargo.toInt() >= 50)
+    val permission = (isDamaged.toBoolean() == false && worker.toInt() in rangeBoardWorker && cargo.toInt() > MIN_BOARDS_PROVISION) ||
+            (worker.toInt() == perfectQuantityWorker && isWeatherGood.toBoolean() == true && cargo.toInt() > MIN_BOARDS_PROVISION)
 
     println("Корабль может отправляться в плавание :$permission")
 }
+const val MIN_BOARDS_PROVISION = 50
