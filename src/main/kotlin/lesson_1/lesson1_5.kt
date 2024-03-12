da55f82a78d0ae76f349a9c2be0380ef2188fa1f
mac <afanasevlad@hotmail.com>
2024-03-12 09:57:24 +0300

    KS-1-5 add const val

--- src/main/kotlin/lesson_1/lesson1_5.kt
+++ src/main/kotlin/lesson_1/lesson1_5.kt
@@ -3,13 +3,13 @@ package org.example.lesson_1
 fun main() {
 
     val secondInSpase: Short = 6480
-    val minInHour = 60
-    val secInHour = 3600
 
-    val hour: Int = secondInSpase / secInHour
-    val minute: Int = secondInSpase / minInHour - hour * minInHour
-    val second: Int = secondInSpase % minInHour
+    val hour: Int = secondInSpase / SEC_IN_HOUR
+    val minute: Int = secondInSpase / MIN_IN_HOUR - hour * MIN_IN_HOUR
+    val second: Int = secondInSpase % MIN_IN_HOUR
 
     println(String.format("%02d:%02d:%02d", hour, minute, second))
 
 }
+const val MIN_IN_HOUR = 60
+const val SEC_IN_HOUR = 3600
