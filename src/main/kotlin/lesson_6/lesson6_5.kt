f93113f558bc0b0ab2de5c25ab3b42b53b7b465c
mac <afanasevlad@hotmail.com>
2024-03-12 18:53:10 +0300

    KS-6-5 добавлен тест на робота

--- src/main/kotlin/lesson_6/lesson6_5.kt
+++ src/main/kotlin/lesson_6/lesson6_5.kt
@@ -1,4 +1,7 @@
 package org.example.lesson_6
+
+import kotlin.random.Random
+
 fun main() {
 
     println("Придумайте логин:")
@@ -12,6 +15,9 @@ fun main() {
     var login: String
     var password: String
 
+    var attempts = 3
+
+
     do {
         println("Введите логин")
         login = readln()
@@ -19,8 +25,28 @@ fun main() {
         println("Введите пароль")
         password = readln()
 
-    } while (login != insertLogin && password != insertPassword)
+        println("Докажите что вы не робот")
+        val numForTest1 = getNumTest(MIN_NUM_TEST, MAX_NUM_TEST)
+        val numForTest2 = getNumTest(MIN_NUM_TEST, MAX_NUM_TEST)
+        val sum = numForTest1 + numForTest2
+
+        println("Решите пример $numForTest1 + $numForTest2 =")
+        val inputSum = readln().toInt()
+
+        attempts--
+        println(attempts)
+
+    } while (login != insertLogin && password != insertPassword && sum != inputSum && attempts > 0)
 
     println("Успешно")
 
 }
+
+fun getNumTest(minNum: Int, maxNum: Int): Int {
+
+    return Random.nextInt(minNum, maxNum + 1)
+
+}
+
+const val MIN_NUM_TEST = 0
+const val MAX_NUM_TEST = 9
