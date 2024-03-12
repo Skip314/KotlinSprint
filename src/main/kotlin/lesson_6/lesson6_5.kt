cdd25c645ed6ab10a1aa53591082a62b2a35d634
mac <afanasevlad@hotmail.com>
2024-03-12 13:39:02 +0300

    KS-6-5 paste ks6-1

--- src/main/kotlin/lesson_6/lesson6_5.kt
+++ src/main/kotlin/lesson_6/lesson6_5.kt
@@ -1,7 +1,26 @@
 package org.example.lesson_6
-
 fun main() {
 
+    println("Придумайте логин:")
+    val insertLogin = readln()
 
+    println("Придумайте пароль")
+    val insertPassword = readln()
+
+    println("Аккаунт $insertLogin успешно создан!")
+
+    var login: String
+    var password: String
+
+    do {
+        println("Введите логин")
+        login = readln()
+
+        println("Введите пароль")
+        password = readln()
+
+    } while (login != insertLogin && password != insertPassword)
+
+    println("Успешно")
 
 }
