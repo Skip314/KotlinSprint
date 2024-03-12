ad18ee9c95039cb5ce4dd16a97c45cf54a3d677e
mac <afanasevlad@hotmail.com>
2024-03-12 19:42:21 +0300

    KS-2-4 add chance

--- src/main/kotlin/lesson_2/lesson2_4.kt
+++ src/main/kotlin/lesson_2/lesson2_4.kt
@@ -4,10 +4,11 @@ fun main() {
 
     val cristalOre: Int = 7
     val ironOre: Int = 11
+    val chance = 20
 
-    val bafCristalOre = cristalOre * 0.2
+    val bafCristalOre = cristalOre * chance / 100
     println("Добыто ${bafCristalOre.toInt()} бонусной кристалической руды")
-    val bafIronOre = ironOre * 0.2
+    val bafIronOre = ironOre * chance / 100
     println("Добыто ${bafIronOre.toInt()} бонусной железной руды")
 
 }
