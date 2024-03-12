3bdb4852714cf315bef555c4048301f378451ba8
mac <afanasevlad@hotmail.com>
2024-03-12 13:12:39 +0300

    KS-6-4 fix

--- src/main/kotlin/lesson_6/lesson6_4.kt
+++ src/main/kotlin/lesson_6/lesson6_4.kt
@@ -4,18 +4,19 @@ fun main() {
 
     val rateNum = 1..9
     var attempts = 5
-    val vinNum = 3
+    val vinNum = 9
     var indikator: Boolean = false
     var inputNum: Int
 
-    while (attempts > 0 && indikator == true) {
+    while (attempts > 0 && indikator != true) {
         attempts--
         println("Угадайте число $rateNum")
         inputNum = readln().toInt()
         if (inputNum == vinNum) {
             indikator = true
-            println("You vin")
-        }
+            println("Это была великолепная игра!")
+        }else println("Неверно")
     }
+    println("Было загадано число $vinNum")
 
 }
