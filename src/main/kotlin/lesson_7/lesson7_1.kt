22dff491f736bdae82faf38452e11443a46edd51
mac <afanasevlad@hotmail.com>
2024-03-14 22:47:21 +0300

    7-1 create

--- src/main/kotlin/lesson_7/lesson7_1.kt
+++ src/main/kotlin/lesson_7/lesson7_1.kt
@@ -3,8 +3,19 @@ package org.example.lesson_7
 fun main() {
 
     val rangeNum = 0..9
-    val rangeString = "a".."z"
+    val rangeString: CharRange = 'a'..'z'
 
+    val pas = listOf(
+        getStringRandom(rangeString), getIntRandom(rangeNum),
+        getStringRandom(rangeString), getIntRandom(rangeNum),
+        getStringRandom(rangeString), getIntRandom(rangeNum),
+        )
+    println("${pas[0]}${pas[1]}${pas[2]}${pas[3]}${pas[4]}${pas[5]}")
 
-
+}
+fun getIntRandom(range: IntRange): String {
+    return range.random().toString()
+}
+fun getStringRandom(range: CharRange): String {
+    return range.random().toString()
 }
