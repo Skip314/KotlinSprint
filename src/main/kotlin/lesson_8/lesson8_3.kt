6f49f5363796a35515fcc80832a13c6cd572d9ae
mac <afanasevlad@hotmail.com>
2024-03-16 21:35:48 +0300

    KS-8-3 create

---
+++ src/main/kotlin/lesson_8/lesson8_3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val arrayOfIngridient = arrayOf("огурец", "помидор", "фета")
+
+    print("Введите ингридиент для поиска: ")
+    val inputIngredientSearch = readLine()
+
+    val searching = arrayOfIngridient.indexOf(inputIngredientSearch)
+
+    if (searching >= 0 ) {
+        println("Ингридиент $inputIngredientSearch в списке есть")
+        }else println("Такого ингридиента в списке нет")
+
+
+}
