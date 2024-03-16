0896dd9abcff3252d896d8728ea53312ee809311
mac <afanasevlad@hotmail.com>
2024-03-16 13:31:24 +0300

    KS-6-5 добавление return выноска attempts во внутрь while

--- src/main/kotlin/lesson_6/lesson6_5.kt
+++ src/main/kotlin/lesson_6/lesson6_5.kt
@@ -4,50 +4,41 @@ import kotlin.random.Random
 
 fun main() {
 
-    println("Придумайте логин:")
+    val numForTestRange = 0..9
+
+    print("Придумайте логин: ")
     val insertLogin = readln()
 
-    println("Придумайте пароль")
+    print("Придумайте пароль: ")
     val insertPassword = readln()
 
     println("Аккаунт $insertLogin успешно создан!")
 
-    var login: String
-    var password: String
-
     var attempts = 3
 
-
     do {
-        println("Введите логин")
-        login = readln()
+        print("Введите Ваш логин: ")
+        val login = readln()
 
-        println("Введите пароль")
-        password = readln()
+        print("Введите Ваш пароль: ")
+        val password = readln()
 
         println("Докажите что вы не робот")
-        val numForTest1 = getNumTest(MIN_NUM_TEST, MAX_NUM_TEST)
-        val numForTest2 = getNumTest(MIN_NUM_TEST, MAX_NUM_TEST)
+        val numForTest1 = numForTestRange.random()
+        val numForTest2 = numForTestRange.random()
         val sum = numForTest1 + numForTest2
 
-        println("Решите пример $numForTest1 + $numForTest2 =")
+        print("Решите пример $numForTest1 + $numForTest2 = ")
         val inputSum = readln().toInt()
 
         attempts--
 
-        if (attempts == 0) println("Доступ запрещен")
-        else if (attempts == 1) println("Осталась $attempts попытка")
-        else println("Осталось $attempts попытки")
+        if (attempts == 0) {
+            println("Доступ запрещен")
+            return
+        }
 
-    } while (login != insertLogin || password != insertPassword || sum != inputSum || attempts != 0)
+    } while (login != insertLogin || password != insertPassword || sum != inputSum)
 
+    println("Добро пожаловать!")
 }
-
-fun getNumTest(minNum: Int, maxNum: Int): Int {
-
-    return Random.nextInt(minNum, maxNum + 1)
-
-}
-
-const val MIN_NUM_TEST = 0
-const val MAX_NUM_TEST = 9
