1d23de7356322df14e4fa51e4c9692e89f3fa960
mac <afanasevlad@hotmail.com>
2024-03-16 22:06:03 +0300

    KS-8-4 create

---
+++ src/main/kotlin/lesoon_8/lesson8_4.kt
@@ -0,0 +1,30 @@
+package org.example.lesoon_8
+
+fun main() {
+
+    val arrayOfIngridient = arrayOf("огурец", "помидор", "фета")
+    println(printArray(arrayOfIngridient))
+
+    print("Введите ингридиент, который вы хотели бы заменить: ")
+    val inputSearchIngridient = readln()
+
+    val search = arrayOfIngridient.indexOf(inputSearchIngridient)
+
+    if (search < 0) {
+
+        println("Такого ингридиента нет")
+        return
+    }
+
+    print("Какой ингридиент вы хотите добавить: ")
+    val inputNewIngridient = readln()
+
+    arrayOfIngridient.set(search, inputNewIngridient)
+    println("Готово! Вы сохранили следующий список:")
+    printArray(arrayOfIngridient)
+}
+fun printArray(array: Array<String>) {
+
+    for(i in array)
+        print("|$i| ")
+}
