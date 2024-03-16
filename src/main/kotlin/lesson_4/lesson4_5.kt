824d116f4bfe97f8b30951cd51ec285578785067
mac <afanasevlad@hotmail.com>
2024-03-16 14:31:16 +0300

    KS-4-5 переменные перенесены в константы

--- src/main/kotlin/lesson_4/lesson4_5.kt
+++ src/main/kotlin/lesson_4/lesson4_5.kt
@@ -2,25 +2,27 @@ package org.example.lesson_4
 
 fun main() {
 
-    println("Укажите поврежден ли корабль (true/false)")
-    val isDamaged = readln()
+    print("Укажите поврежден ли корабль (true/false) ")
+    val isDamaged = readln().toBoolean()
 
-    println("Введите колличество экипажа")
-    val worker = readln()
+    print("Введите колличество экипажа: ")
+    val worker = readln().toInt()
 
-    println("Укажите количество ящиков провизии")
-    val cargo = readln()
+    print("Укажите количество ящиков провизии: ")
+    val cargo = readln().toInt()
 
-    println("Укажите погоду (true/false)")
-    val isWeatherGood = readln()
+    print("Укажите погоду (true/false) ")
+    val isWeatherGood = readln().toBoolean()
 
-    val rangeBoardWorker = 55 .. 70
-    val perfectQuantityWorker = 70
+    val rangeBoardWorker = 55..70
 
-
-    val permission = (isDamaged.toBoolean() == false && worker.toInt() in rangeBoardWorker && cargo.toInt() > MIN_BOARDS_PROVISION) ||
-            (worker.toInt() == perfectQuantityWorker && isWeatherGood.toBoolean() == true && cargo.toInt() > MIN_BOARDS_PROVISION)
+    val permission = (isDamaged == BOARD_DAMAGED && worker in rangeBoardWorker && cargo > MIN_BOARDS_PROVISION) ||
+            (worker == GOOD_QUANTITY_WORKER && isWeatherGood == WEATHER_GOOD && cargo > MIN_BOARDS_PROVISION)
 
     println("Корабль может отправляться в плавание :$permission")
 }
+
 const val MIN_BOARDS_PROVISION = 50
+const val WEATHER_GOOD = true
+const val BOARD_DAMAGED = false
+const val GOOD_QUANTITY_WORKER = 70
