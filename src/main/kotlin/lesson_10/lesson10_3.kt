497bcde4b7a7433dc45ee1ab6d4ef294522d777a
mac <afanasevlad@hotmail.com>
2024-03-18 18:40:33 +0300

    KS-10-3 create

--- src/main/kotlin/lesson_10/lesson10_3.kt
+++ src/main/kotlin/lesson_10/lesson10_3.kt
@@ -2,18 +2,21 @@ package org.example.lesson_10
 
 fun main() {
 
-    println("Введите желаемое количество символов в пароле: ")
+    print("Введите желаемое количество символов в пароле: ")
     val inputQuantitySimvol = readln().toInt()
 
     getRandomSimvol(inputQuantitySimvol)
 }
+
 fun getRandomSimvol(quantity: Int) {
 
     var quantityRange = 1..quantity
-    val specSimvolList = listOf('!','"','#')
+    val specSimvolList = listOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ')
     val range = 0..9
 
-    for(i in quantityRange) {
+    for (i in quantityRange) {
 
+        if (i % 2 != 0) print(range.random())
+        else print(specSimvolList.random())
     }
 }
