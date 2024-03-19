18f06fe0c76c945a842a9bf97b36bdec97be0beb
mac <afanasevlad@hotmail.com>
2024-03-19 15:31:46 +0300

    KS-11-1 create

---
+++ src/main/kotlin/lesson_11/lesson11_1.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_11
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+    fun printUser() {
+        println("$id, $login, $password, $email")
+    }
+}
+
+fun main() {
+
+    val user1 = User(0, "Vlad", "123", "11@mail.ru")
+    val user2 = User(1, "Ivan", "321", "i@mail.ru")
+
+    user1.printUser()
+    user2.printUser()
+}
