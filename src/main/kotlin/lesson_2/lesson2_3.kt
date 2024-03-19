d5fb6b6efa491fdb71b721e6fa26052edd52fd4a
mac <afanasevlad@hotmail.com>
2024-03-19 20:15:41 +0300

    KS-2-3 fix spase

--- src/main/kotlin/lesson_2/lesson2_3.kt
+++ src/main/kotlin/lesson_2/lesson2_3.kt
@@ -4,16 +4,16 @@ fun main() {
 
     val startTime = "9:39"
     val travelTime = 457
-    val minInHour = 60
 
     val hourStart = startTime.substringBefore(':')
 
     val minuteStart = startTime.substring(startTime.lastIndexOf(':') + 1)
 
-    val finishTime: Int = hourStart.toInt() * minInHour + minuteStart.toInt() + travelTime
+    val finishTime: Int = hourStart.toInt() * MIN_IN_HOUR + minuteStart.toInt() + travelTime
 
-    val finishHour = finishTime / minInHour
-    val finishMinute = finishTime % minInHour
+    val finishHour = finishTime / MIN_IN_HOUR
+    val finishMinute = finishTime % MIN_IN_HOUR
 
     println(String.format("%02d:%02d", finishHour, finishMinute))
 }
+const val MIN_IN_HOUR = 60
