698e279fec4bba093862389abf32536c264bf41f
mac <afanasevlad@hotmail.com>
2024-03-21 13:00:02 +0300

    KS-11-3 update

--- src/main/kotlin/lesson_11/lesson11_3.kt
+++ src/main/kotlin/lesson_11/lesson11_3.kt
@@ -4,20 +4,54 @@ package org.example.lesson_11
 class Room(
     val cover: String,
     val name: String,
-    var listOfMember: MutableList<Member>,
 ) {
+
+    val listOfMember: MutableList<Member> = mutableListOf()
     fun addMember() {
 
-        println("Добавить в комнату участника: ")
-        val inputMember = readln()
+        println("Добавить в комнату участника")
+        print("Введите имя: ")
+        val inputName = readln()
+        print("Введите аватар: ")
+        val inputAvatar = readln()
+        val status = selectStatus()
+        val member = Member(inputName, inputAvatar, status)
+        listOfMember.add(member)
+    }
 
-        listOfMember.add(inputMember)
+    fun selectStatus():String {
 
+        print("Выберите статус: “разговаривает” - 1, “микрофон выключен” - 2, “пользователь заглушен” - 3 ")
+        var inputStatus: String = readln()
+        when (inputStatus) {
+            "1" -> inputStatus = "разговаривает"
+            "2" -> inputStatus = "микрофон выключен"
+            "3" -> inputStatus = "пользователь заглушен"
+            else -> {
+                print("Выбор не верен, попробуйте еще раз: “разговаривает” - 1, “микрофон выключен” - 2, “пользователь заглушен” - 3: ")
+                inputStatus = readln()
+            }
+        }
+
+        return inputStatus
     }
 
     fun editStatus() {
 
-        println()
+        print("Введите ваше имя")
+        val inputName = readln()
+        val member = listOfMember.find { it.name == inputName }
+        if (member != null) member.status = selectStatus()
+
+    }
+
+    fun printMember(room: Room) {
+
+        println("В комнате ${room.name} следующие участники:")
+        for (member in listOfMember) {
+
+            println("Аватар: ${member.avatar} , Имя: ${member.name} , Статус: ${member.status}")
+        }
     }
 }
 
@@ -25,7 +59,7 @@ class Member(
 
     val name: String,
     val avatar: String,
-    val status: String,
+    var status: String,
 ) {
     fun printMember() {
 
@@ -33,17 +67,20 @@ class Member(
         println(avatar)
         println(status)
     }
-
 }
 
 fun main() {
 
-    val listOfMemberInRoom1 = mutableListOf<Member>()
-    var room1 = Room("Программист", "Котлин", listOfMemberInRoom1)
-    var member1 = Member("Vlad", ":|", "разговаривает")
-    var member2 = Member("Ivan", ";)", "пользователь заглушен")
+    val room = Room("Picture", "Kotlin")
 
-    listOfMemberInRoom1.add(member1)
-    listOfMemberInRoom1.add(member2)
-    println(listOfMemberInRoom1)
+    val member1 = Member("Vlad",":)","разговаривает")
+    room.listOfMember.add(member1)
+
+    room.printMember(room)
+
+    room.addMember()
+
+    room.editStatus()
+
+    room.printMember(room)
 }
