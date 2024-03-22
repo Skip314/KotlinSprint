136bf4337f5ddfed874518f96270d306dee77d86
mac <afanasevlad@hotmail.com>
2024-03-22 15:59:43 +0300

    KS-4-3 create const

--- src/main/kotlin/lesson_4/lesson4_3.kt
+++ src/main/kotlin/lesson_4/lesson4_3.kt
@@ -3,15 +3,20 @@ package org.example.lesson_4
 fun main() {
 
     val weatherToday: String = "sun"
-    val openTent: Boolean = true
+    val isOpenTent: Boolean = true
     val air: Int = 20
     val season: String = "winter"
 
     println(
         """
         Благоприятные ли условия сейчас для роста бобовых?
-        ${weatherToday == "sun" && openTent == true && air == 20 && season != "winter"}
+        ${weatherToday == WEATHER_TODAY && isOpenTent == IS_OPEN_TENT && air == WET_AIR && season != NOT_A_SEASON}
         """.trimIndent()
     )
 
 }
+
+const val WEATHER_TODAY = "sum"
+const val IS_OPEN_TENT = true
+const val WET_AIR = 20
+const val NOT_A_SEASON = "winter"
