879d9a7abc10de5625c6debcf57dbcf40c7bea55
mac <afanasevlad@hotmail.com>
2024-03-22 13:36:10 +0300

    KS-11-5

--- src/main/kotlin/lesson_11/lesson11_5.kt
+++ src/main/kotlin/lesson_11/lesson11_5.kt
@@ -4,53 +4,67 @@ import javax.sound.midi.MetaMessage
 
 class Forum(
 
-    //понятия не имею зачем он
-)
+) {
+    var lastId = 0
+    val listOfUser = mutableListOf<User5>()
+    val listOfMessage = mutableListOf<MessageForum>()
+
+    fun createNewUser(name: String, id: Int = lastId) {
+
+        listOfUser.add(User5(id, name))
+        println("Создан пользователь $name создан с id: $lastId")
+        listOfMessage.add(MessageForum(lastId, "Привет, меня зовут $name"))
+        lastId++
+    }
+
+    fun printUser() {
+
+        for (i in listOfUser)
+            println("${i.userName}, id: ${i.userId}")
+    }
+
+    fun createNewMessage(userId: Int, message: String) {
+
+        if (listOfUser.find { it.userId == userId } == null) return
+        listOfMessage.add(MessageForum(userId, message))
+    }
+
+    fun printThread() {
+
+        for (i in listOfMessage) {
+            val name = listOfUser.find { it.userId == i.authorId }
+            if (name != null) {
+                println("${name.userName} : ${i.message}")
+            }
+        }
+    }
+}
 
 class User5(
 
     var userId: Int,
     var userName: String,
-) {
-    fun createNewUser(id: Int): User5 {
-
-        print("Введите имя нового пользователя: ")
-        var i = id
-        val name = readln()
-        val user1 = User5(id, name)
-        return user1
-    }
-}
+)
 
 class MessageForum(
 
     val authorId: Int,
     val message: String,
-){
-    fun createNewMessage(id: Int){
-
-        println("для создания сообщения введите ваш id: ")
-        val inputId = readln().toInt()
-        if(id <= inputId) return
-
-        print("Ваше сообщение: ")
-        val inputMessage = readln()
-        val message: MessageForum(inputId, inputMessage)
-    }
-}
+)
 
 fun main() {
 
-    var id = 0
+    val forum = Forum()
 
-    val user1 = User5(0, "")
-    user1.createNewUser(id)
-    id ++
+    forum.createNewUser("Ivan")
+    forum.createNewMessage(0,"Hi")
 
-    val user2 = User5(0,"")
-    user2.createNewUser(id)
-    id ++
+    forum.createNewUser("Vlad")
+    forum.createNewMessage(1, "My name is Vlad")
 
-    println("${user1.userId}, ${user1.userName}")
-    println("${user2.userId}, ${user2.userName}")
+    forum.createNewMessage(0, "ok")
+
+    forum.createNewUser("Fedor")
+    forum.createNewMessage(2, "I am Fedor")
+    forum.printThread()
 }
