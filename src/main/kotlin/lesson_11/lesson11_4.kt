18315acdf8d297302de4d8b6bf191aba3d4fc563
mac <afanasevlad@hotmail.com>
2024-03-22 11:41:36 +0300

    KS-11-4

--- src/main/kotlin/lesson_11/lesson11_4.kt
+++ src/main/kotlin/lesson_11/lesson11_4.kt
@@ -5,13 +5,14 @@ class Obj(
     val category: String,
     val name: String,
     val picture: String,
-    val recept: List<String>,
-    val tegFavorites: Boolean = false
+    val tegFavorites: Boolean = false,
 
-)
+){
+    val recept: MutableList<String> = mutableListOf()
+}
 fun main() {
 
     val burgerMiC = Obj(
-        0,"burgers","бургер с грибами и сыром","burger.png", listOf("Творог - 350г")
+        0,"burgers","бургер с грибами и сыром","burger.png"
     )
 }
