a5332bb299740e148eb7ef8abf578cbfdf6013a0
Afana <afanasevlad@mail.ru>
2024-03-25 23:29:03 +0300

    KS-7-2

--- src/main/kotlin/lesson_7/lesson7_2.kt
+++ src/main/kotlin/lesson_7/lesson7_2.kt
@@ -3,12 +3,13 @@ package org.example.lesson_7
 fun main() {
 
     val code = (1000..9999).random()
+    var inputCode: Int
 
     do {
 
         println("Ваш код авторизации: $code")
         print("Введите код: ")
-        val inputCode = readln().toInt()
+        inputCode = readln().toInt()
 
     } while (code != inputCode)
 
