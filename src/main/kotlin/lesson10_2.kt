02cb0db8b451d998385beb3424a3e334e22c28f0
Afana <afanasevlad@mail.ru>
2024-03-26 20:20:57 +0300

    KS-10-2

--- src/main/kotlin/lesson10_2.kt
+++ src/main/kotlin/lesson10_2.kt
@@ -10,10 +10,10 @@ fun main() {
     print("Придумайте пароль: ")
     val pas = readln()
 
-    if (checkLength(login)&& checkLength(pas) == true) {
+    if (checkLength(login) && checkLength(pas)) {
         println("Добро пожаловать!")
         return
-    }else println("Логин или пароль недостаточно длинные")
+    } else println("Логин или пароль недостаточно длинные")
 }
 
 fun checkLength(obj: String): Boolean = obj.length >= 4
