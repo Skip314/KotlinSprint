e97b4511e8d5e8469314af1887e1dccd87a5f65f
mac <afanasevlad@hotmail.com>
2024-03-28 14:02:56 +0300

    KS-12-1 create

--- src/main/kotlin/lesson_12/lesson12_1.kt
+++ src/main/kotlin/lesson_12/lesson12_1.kt
@@ -1,29 +1,28 @@
 package org.example.lesson_12
 
-class weatherDay() {
+class WeatherDay() {
 
     var dayTemp = 0
     var nightTemp = 0
-    var isOsadki = false
+    var isRain = false
 
     fun printWeather() {
 
-        println("Температура днем: ${dayTemp}, ночью ${nightTemp}, осадки: ${isOsadki}")
+        println("Температура днем: ${dayTemp}, ночью ${nightTemp}, осадки: ${isRain}")
     }
 }
 
-
 fun main() {
 
-    val day1 = weatherDay()
+    val day1 = WeatherDay()
     day1.dayTemp = 24
     day1.nightTemp = 11
-    day1.isOsadki = true
+    day1.isRain = true
 
-    val day2 = weatherDay()
+    val day2 = WeatherDay()
     day2.dayTemp = 16
     day2.nightTemp = 0
-    day2.isOsadki = false
+    day2.isRain = false
 
     day1.dayTemp = 22
 
