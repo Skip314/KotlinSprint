78bb74d32f52286ad6d372729df1aae44d168986
mac <afanasevlad@hotmail.com>
2024-03-29 14:50:09 +0300

    KS-14-2 create

--- src/main/kotlin/lesson_14/lesson14_1.kt
+++ src/main/kotlin/lesson_14/lesson14_1.kt
@@ -7,11 +7,29 @@ open class Liner(
     val cargo: Int = 200,
     val passenger: Int = 30,
     val iceBreaker: Boolean = false,
-)
+){
+    open fun download() {
+
+        println("Лайнер $name выдвигает горизонтальный трап со шкафута для погрузки")
+    }
+
+    fun showClass(){
+
+        println("Название корабля: $name, скорость: $speed, грузоподъёмность: " +
+                "$cargo, пассажировместимость: $passenger, ледокол: $iceBreaker")
+        println()
+    }
+}
 
 class IceBreakerShip(
     name: String,
-) : Liner(name, speed = 8, cargo = 50, passenger = 10, iceBreaker = true)
+) : Liner(name, speed = 8, cargo = 50, passenger = 10, iceBreaker = true){
+
+    override fun download() {
+
+        println("Ледокол $name открывает ворота со стороны кормы для погрузки")
+    }
+}
 
 class CargoShip(
     name: String,
@@ -19,12 +37,26 @@ class CargoShip(
     cargo: Int = 500,
     passenger: Int = 15,
     iceBreaker: Boolean = false,
-) : Liner(name, speed, cargo, passenger, iceBreaker)
+) : Liner(name, speed, cargo, passenger, iceBreaker){
+
+    override fun download() {
+
+        println("Грузовой корабль $name активирует погрузочный кран для погрузки")
+    }
+}
 
 fun main() {
 
     val ship1 = Liner("Legion")
+    ship1.download()
+    ship1.showClass()
+
     val ship2 = IceBreakerShip("Kapitan")
+    ship2.download()
+    ship2.showClass()
+
     val ship3 = CargoShip("Front")
+    ship3.download()
+    ship3.showClass()
 }
 
