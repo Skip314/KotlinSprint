b81515cbb9dc297af122b175e70f7c6f461c8aee
mac <afanasevlad@hotmail.com>
2024-03-31 11:35:53 +0300

    KS-14-2 create

--- src/main/kotlin/lesson_14/lesson14_1.kt
+++ src/main/kotlin/lesson_14/lesson14_1.kt
@@ -1,29 +1,30 @@
 package org.example.lesson_14
 
 open class Liner2(
-
     val name: String,
     val speed: Int = 10,
     val cargo: Int = 200,
     val passenger: Int = 30,
     val iceBreaker: Boolean = false,
-){
+) {
     open fun download() {
 
         println("Лайнер $name выдвигает горизонтальный трап со шкафута для погрузки")
     }
 
-    fun showClass(){
+    fun showClass() {
 
-        println("Название корабля: $name, скорость: $speed, грузоподъёмность: " +
-                "$cargo, пассажировместимость: $passenger, ледокол: $iceBreaker")
+        println(
+            "Название корабля: $name, скорость: $speed, грузоподъёмность: " +
+                    "$cargo, пассажировместимость: $passenger, ледокол: $iceBreaker"
+        )
         println()
     }
 }
 
 class IceBreakerShip2(
     name: String,
-) : Liner2(name, speed = 8, cargo = 50, passenger = 10, iceBreaker = true){
+) : Liner2(name, speed = 8, cargo = 50, passenger = 10, iceBreaker = true) {
 
     override fun download() {
 
@@ -33,11 +34,7 @@ class IceBreakerShip2(
 
 class CargoShip2(
     name: String,
-    speed: Int = 9,
-    cargo: Int = 500,
-    passenger: Int = 15,
-    iceBreaker: Boolean = false,
-) : Liner2(name, speed, cargo, passenger, iceBreaker){
+) : Liner2(name, speed = 9, cargo = 500, passenger = 15, iceBreaker = false) {
 
     override fun download() {
 
@@ -59,4 +56,3 @@ fun main() {
     ship3.download()
     ship3.showClass()
 }
-
