6258f4f3c09118cf2f0814e7ab59ec6d4ffb46ec
mac <afanasevlad@hotmail.com>
2024-04-10 10:32:14 +0300

    KS-22-3 create

--- src/main/kotlin/lesson_22/lesson22_3.kt
+++ src/main/kotlin/lesson_22/lesson22_3.kt
@@ -10,12 +10,6 @@ fun main() {
 
     val rBook = DataBook("Regular", "Los'", 10)
 
-    val name = rBook.name
-    println(name)
-
-    val author = rBook.author
-    println(author)
-
-    val quantity = rBook.quantity
-    println(quantity)
+    val (bookName, bookAuthor, booksQuantity) = rBook
+    println("$bookName, $bookAuthor, $booksQuantity")
 }
