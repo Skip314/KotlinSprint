9cfb91f4c1036e6671b386f98fedd7c3a6975471
mac <afanasevlad@hotmail.com>
2024-04-11 10:37:38 +0300

    KS-5-5 refactor

--- src/main/kotlin/lesson_5/lesson5_5.kt
+++ src/main/kotlin/lesson_5/lesson5_5.kt
@@ -1,13 +1,11 @@
 package org.example.lesson_5
 
-import kotlin.random.Random
-
 fun main() {
 
     val vinNum = getVinNum()
 
     print("Введите поочередно три цифры: ")
-    val inputNum = listOf( readln().toInt(), readln().toInt(), readln().toInt())
+    val inputNum = listOf(readln().toInt(), readln().toInt(), readln().toInt())
 
     println(checkNum(inputNum, vinNum))
     println("Выйгрышные числа: ${vinNum.joinToString()}")
@@ -18,7 +16,7 @@ fun getVinNum(): List<Int> {
     val rangeVinNum = MIN_VIN_NUM..MAX_VIN_NUM
     val listOfVinNum = mutableSetOf<Int>()
 
-    while (listOfVinNum.size != 3){
+    while (listOfVinNum.size != 3) {
 
         listOfVinNum.add(rangeVinNum.random())
     }
@@ -29,7 +27,7 @@ fun checkNum(userNum: List<Int>, vinNum: List<Int>): String {
 
     val check = vinNum.intersect(userNum)
 
-    return when(check.size){
+    return when (check.size) {
         3 -> "Вы угадали все числа и выйграли ДЖЕКПОТ!"
         2 -> "Вы угадали 2 числа и получаете крупный приз"
         1 -> "Вы угадали 1 число и получаете утешительный приз"
