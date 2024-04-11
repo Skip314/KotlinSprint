dccbe2f70ce0109b80b9df4fb0779b35784f7a6c
mac <afanasevlad@hotmail.com>
2024-04-11 11:35:26 +0300

    KS-14-5 refactor

--- src/main/kotlin/lesson_14/lesson14_5.kt
+++ src/main/kotlin/lesson_14/lesson14_5.kt
@@ -3,32 +3,65 @@ package org.example.lesson_14
 var userId = 0
 var messageId = 0
 
-data class Chat(
-    val name: String,
-    val chatName: String,
+open class Chat(
+    open val name: String,
 ) {
 
-    val listOfMessage = mutableListOf<Message>()
+    companion object {
+        val mapOfMessage = mutableMapOf<Int, Message>()
+    }
 
     open fun addMessage(text: String) {
 
         val message = Message(messageId, this.name, text)
+        mapOfMessage[messageId] = message
 
         messageId++
     }
 
     fun addThreadMessage() {}
+
+    fun printChat() {
+
+        mapOfMessage.forEach { (key, value) ->
+            println("id:$key ${value.author}: ${value.text}")
+        }
+    }
 }
 
-
-
 data class User(
-    val name: String,
-    val userId: Int
-)
+    override val name: String,
+) : Chat(name) {
 
-data class Message(
+    val id: Int = userId
+
+    init {
+        userId++
+    }
+}
+
+open class Message(
     val messageId: Int,
     val author: String,
     val text: String,
 )
+
+class ChildMessage(
+    messageId: Int,
+    author: String,
+    text: String,
+    val childMessageId: Int
+) : Message(messageId, author, text)
+
+fun main() {
+
+    val chat = Chat("Чат")
+
+    val user1 = User("Fedor")
+    user1.addMessage("Привет, я Fedor")
+
+    val user2 = User("Oleg")
+    user2.addMessage("Hi")
+
+    chat.printChat()
+}
