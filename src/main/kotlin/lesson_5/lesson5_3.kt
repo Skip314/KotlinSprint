c88e11fe37b938c1e91f82866dada79563404810
mac <afanasevlad@hotmail.com>
2024-04-12 09:42:13 +0300

    KS-5-3 create

--- src/main/kotlin/lesson_5/lesson5_3.kt
+++ src/main/kotlin/lesson_5/lesson5_3.kt
@@ -2,22 +2,22 @@ package org.example.lesson_5
 
 fun main() {
 
-    val vinNum1 = "31"
-    val vinNum2 = "42"
+    val vinNum1 = 31
+    val vinNum2 = 42
 
     println("""
         Добро пожаловать в лоторею!
         Угадайте два числа от 0 до 42 и выйграйте приз!
-        Введите ваши числа
+        Введите ваши числа: 
     """.trimIndent())
 
-    val inputNum1 = readLine().toString()
-    val inputNum2 = readLine().toString()
+    val inputNum1 = readln().toInt()
+    val inputNum2 = readln().toInt()
 
     println(compareNum(vinNum1, vinNum2, inputNum1, inputNum2))
 
 }
-fun compareNum(vinNum1: String, vinNum2: String, num1: String, num2: String): String {
+fun compareNum(vinNum1: Int, vinNum2: Int, num1: Int, num2: Int): String {
 
     return when {
         (num1 == vinNum1 || num2 == vinNum1) && (num1 == vinNum2 || num2 == vinNum2) -> "Поздравляем! Вы выиграли главный приз!"
