2760d991b12baca98500651fde196a0e56a83520
mac <afanasevlad@hotmail.com>
2024-04-12 09:50:59 +0300

    KS-6-3 create

---
+++ src/main/kotlin/lesson_6/lesson6_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_6
+
+fun main() {
+
+    print("Введите количество секунд, которое надо засечь: ")
+
+    val insertSecond = readln().toInt()
+    var remSecond = insertSecond
+
+    while (remSecond != 0) {
+
+        println("Осталось секунд: $remSecond")
+        remSecond--
+        Thread.sleep(1000)
+    }
+
+    println("Прошло секунд: $insertSecond ")
+}
