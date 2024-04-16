3f54f54b73825f7388746c7a1ea4f8285c942ab3
mac <afanasevlad@hotmail.com>
2024-04-16 09:50:07 +0300

    KS-20-1 create

--- src/main/kotlin/lesson_20/lesson20_2.kt
+++ src/main/kotlin/lesson_20/lesson20_2.kt
@@ -3,13 +3,14 @@ package org.example.lesson_20
 class User2(
     val name: String,
     var health: Int,
+    val maxHealth: Int = 100,
 )
 
 fun main() {
 
     val user = User2("player", 64)
 
-    val healingPotion: (user: User2) -> Unit = {user.health = 100}
+    val healingPotion: (user: User2) -> Unit = { user.health = user.maxHealth }
 
     healingPotion(user)
 }
