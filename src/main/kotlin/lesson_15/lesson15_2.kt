44afee9ab9593daf92f4026c21b8d2cadb4f037b
mac <afanasevlad@hotmail.com>
2024-04-16 10:16:53 +0300

    KS-15-2 create

---
+++ src/main/kotlin/lesson_15/lesson15_2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_15
+
+abstract class WeatherStationStatus()
+
+class WeatherServer(){
+
+    fun sendResult(){
+        println("Сообщение отправлено")
+    }
+}
+
+class Temperature()
+
+class PrecipitationAmount()
