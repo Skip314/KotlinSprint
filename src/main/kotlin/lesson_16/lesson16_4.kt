15a6b87aeef0215ad198b66d0d9e7677f33ed3fb
mac <afanasevlad@hotmail.com>
2024-04-20 13:10:21 +0300

    KS

--- src/main/kotlin/lesson_16/lesson16_4.kt
+++ src/main/kotlin/lesson_16/lesson16_4.kt
@@ -4,19 +4,18 @@ class Order(
     private var status: String,
     private val orderNum: Int,
 ) {
-    fun newStatus(newStatus: String) {
+
+    private fun assignNewOrderStatus(newStatus: String) {
         status = newStatus
     }
+
+    fun sendRequest(newStatus: String) = assignNewOrderStatus(newStatus)
 }
 
 fun main() {
 
     val order = Order(ASSEMBLY, 1)
-    changeStatus(order, READY)
-}
-
-fun changeStatus(order: Order, newStatus: String) {
-    order.newStatus(newStatus)
+    order.sendRequest(READY)
 }
 
 const val READY = "Готов к получению"
