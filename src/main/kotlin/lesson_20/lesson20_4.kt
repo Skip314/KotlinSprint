0d4289c020bb4a572b03221d11fbb56464651996
mac <afanasevlad@hotmail.com>
2024-04-22 17:59:26 +0300

    KS

--- src/main/kotlin/lesson_20/lesson20_4.kt
+++ src/main/kotlin/lesson_20/lesson20_4.kt
@@ -4,5 +4,5 @@ fun main() {
 
     val elementsList= mutableListOf("элемент1", "эелмент2", "элемент3", "элемент4")
 
-    val lambdaList = elementsList.map { element -> println(element) }
+    val lambdaList = elementsList.map { element -> println("Нажат элемент: $element") }
 }
