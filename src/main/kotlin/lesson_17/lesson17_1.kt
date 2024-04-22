863b62e2dc4c54cec3630ff821662be4ea75dae8
mac <afanasevlad@hotmail.com>
2024-04-22 09:37:13 +0300

    KS

--- src/main/kotlin/lesson_17/lesson17_1.kt
+++ src/main/kotlin/lesson_17/lesson17_1.kt
@@ -1,6 +1,9 @@
 package org.example.lesson_17
 
-class Task {
+class Task(
+    question: String,
+    answer: String,
+) {
 
     val question: String = "Кто проживает на дне океана?"
         get() = field
