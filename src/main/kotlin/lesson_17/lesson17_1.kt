f8e88996ad67f82075728ddfd5f1f997aaf854f2
mac <afanasevlad@hotmail.com>
2024-04-22 17:52:17 +0300

    KS

--- src/main/kotlin/lesson_17/lesson17_1.kt
+++ src/main/kotlin/lesson_17/lesson17_1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_17
 
 class Task(
-    question: String,
-    answer: String,
+    _question: String = "Кто проживает на дне океана?",
+    _answer: String = "СпанчБоб",
 ) {
 
-    val question: String = "Кто проживает на дне океана?"
+    val question: String = _question
         get() = field
 
-    var answer: String = "СпанчБоб"
+    var answer: String = _answer
         get() = field
         set(value) {
             field = value
