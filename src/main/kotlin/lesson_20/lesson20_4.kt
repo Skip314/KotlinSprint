4ec4ed3a32eeac5c2ac36e84afdba36224ab595a
mac <afanasevlad@hotmail.com>
2024-04-23 16:51:14 +0300

    KS

--- src/main/kotlin/lesson_20/lesson20_4.kt
+++ src/main/kotlin/lesson_20/lesson20_4.kt
@@ -2,7 +2,13 @@ package org.example.lesson_20
 
 fun main() {
 
-    val elementsList= mutableListOf("элемент1", "эелмент2", "элемент3", "элемент4")
+    val elementsList = mutableListOf("элемент1", "эелмент2", "элемент3", "элемент4")
 
-    val lambdaList = elementsList.map { element -> println("Нажат элемент: $element") }
+    val lambdaList = elementsList.map { element -> { println("Нажат элемент: $element") } }
+
+    lambdaList.forEachIndexed { index, lambda ->
+        if (index % 2 == 0) {
+            lambda()
+        }
+    }
 }
