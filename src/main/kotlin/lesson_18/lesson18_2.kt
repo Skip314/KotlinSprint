b084328c3dce77edc93b70029164268bcfd5a4bb
mac <afanasevlad@hotmail.com>
2024-04-24 09:36:00 +0300

    KS

--- src/main/kotlin/lesson_18/lesson18_2.kt
+++ src/main/kotlin/lesson_18/lesson18_2.kt
@@ -1,17 +1,33 @@
 package org.example.lesson_18
 
 open class Dice4(
-    val quantity: Int = 4,
+    open val quantity: Int = 4,
 ) {
-    fun dropDice() {
+    open fun dropDice() {
         val value = (1..quantity).random()
+        println(value)
     }
 }
 
 class Dice6(
-    val quantity: Int = 6,
-)
+    override val quantity: Int = 6,
+) : Dice4(quantity) {
+
+}
 
 class Dice8(
-    val quantity: Int = 8,
-)
+    override val quantity: Int = 8,
+) : Dice4(quantity) {
+
+}
+
+fun main() {
+
+    val dice4 = Dice4()
+    val dice6 = Dice6()
+    val dice8 = Dice8()
+
+    val listOfDice: List<Dice4> = listOf(dice4, dice6, dice8)
+
+    for (dice in listOfDice) dice.dropDice()
+}
