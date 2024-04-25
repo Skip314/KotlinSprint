47bcfd4017fec32b10f7cf1f3001ba4813a6d90f
mac <afanasevlad@hotmail.com>
2024-04-25 22:41:10 +0300

    KS

--- src/main/kotlin/lesson_18/lesson18_5.kt
+++ src/main/kotlin/lesson_18/lesson18_5.kt
@@ -1,12 +1,31 @@
 package org.example.lesson_18
 
-class Screen{
+class Screen {
 
-    fun draw(obj: Point) {
-        print("Объект $obj")
+    fun draw(obj: Point, x: Int, y: Int) {
+        obj.createObj(x, y)
+    }
+
+    fun draw(obj: Ring, x: Int, y: Int) {
+        obj.createObj(x, y)
+    }
+
+    fun draw(obj: Square, x: Int, y: Int) {
+        obj.createObj(x, y)
+    }
+
+    fun draw(obj: Point, x: Float, y: Float) {
+        obj.createObj(x, y)
+    }
+
+    fun draw(obj: Ring, x: Float, y: Float) {
+        obj.createObj(x, y)
+    }
+
+    fun draw(obj: Square, x: Float, y: Float) {
+        obj.createObj(x, y)
     }
 }
-
 
 open class Point(
     val name: String = "точка",
@@ -37,5 +56,6 @@ fun main() {
     val ring = Ring()
     val square = Square()
 
-    screen.draw(point.createObj(1,3))
+    screen.draw(point, 1, 3)
+    screen.draw(square, 12.4f, 3.3f)
 }
