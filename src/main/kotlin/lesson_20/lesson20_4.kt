b5ade68b7f08a79a16b5aac6ee7a8c7fd6d6f353
mac <afanasevlad@hotmail.com>
2024-04-26 10:26:01 +0300

    KS

--- src/main/kotlin/lesson_20/lesson20_4.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_20
-
-fun main() {
-
-    val elementsList= mutableListOf("элемент1", "эелмент2", "элемент3", "элемент4")
-
-    val lambdaList = elementsList.map { element -> println(element) }
-}
