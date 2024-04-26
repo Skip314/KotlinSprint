447188213649649d01909883ad73b93271942973
mac <afanasevlad@hotmail.com>
2024-04-26 15:38:05 +0300

    KS

---
+++ src/main/kotlin/lesson_21/lesson21_3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_21
+
+class Player(
+    val name: String,
+    var health: Int,
+    val maxHealth: Int = 100,
+)
+
+fun main() {
+
+    val player = Player("admin", 100)
+    player.isHealThy()
+}
+
+fun Player.isHealThy(): Boolean {
+    if (this.health == maxHealth)
+        return true
+    else return false
+}
