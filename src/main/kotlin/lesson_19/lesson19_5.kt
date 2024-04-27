c76c1dff4cbb41b4082cbd209090f12b175ce3ec
Afana <afanasevlad@mail.ru>
2024-04-27 22:06:08 +0300

    KS-9-2

--- src/main/kotlin/lesson_19/lesson19_5.kt
+++ src/main/kotlin/lesson_19/lesson19_5.kt
@@ -5,6 +5,7 @@ class User(
     val sex: Sex,
 )
 
+
 enum class Sex {
     MALE,
     FEMALE,
@@ -15,25 +16,25 @@ fun main() {
     println("Введите имя и пол в формате м или ж")
     val cardIndex = mutableListOf<User>()
 
-    while (cardIndex.size != 2) {
 
-        print("Введите имя: ")
-        val name = readln()
-        var inputSex = ""
-
-        while (inputSex != "м" && inputSex != "ж") {
-
-            print("Выберите пол: м - мужской или ж - женский: ")
-            inputSex = readln().lowercase()
-            println(inputSex)
-        }
-        var sex: Sex
-        if (inputSex == "м") sex = Sex.MALE
-        else sex = Sex.FEMALE
-        cardIndex.add(User(name, sex))
-    }
-
-    for (i in cardIndex) {
-        println("${i.name} ${i.sex}")
-    }
 }
+fun readName(): String {
+    print("Введите имя: ")
+    return readln()
+}
+
+fun readSex(): Sex {
+    print("Выберите пол: м - мужской или ж - женский: ")
+    var setSex = readln().lowercase()
+    var sex: Sex? = null
+    when (setSex) {
+        "м" -> sex = Sex.MALE
+        "ж" -> sex = Sex.FEMALE
+        else -> {
+            print("Выберите пол: м - мужской или ж - женский: ")
+            setSex = readln()
+        }
+    }
+    return sex!!
+}
+
