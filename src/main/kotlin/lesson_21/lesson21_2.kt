e99ee4e1f34d4b00426c3d705051623287f33e2d
Afana <afanasevlad@mail.ru>
2024-04-28 12:52:20 +0300

    KS

--- src/main/kotlin/lesson_21/lesson21_2.kt
+++ src/main/kotlin/lesson_21/lesson21_2.kt
@@ -3,14 +3,7 @@ package org.example.lesson_21
 fun main() {
 
     val listNum = listOf(1, 2, 3, 4, 5, 6)
-    listNum.evenNumbersSum()
+    println(listNum.evenNumbersSum())
 }
 
-fun List<Int>.evenNumbersSum() {
-
-    var sum = 0
-    for (num in this) {
-        if (num % 2 == 0) sum += num
-    }
-    println(sum)
-}
+fun List<Int>.evenNumbersSum() = this.filter { it % 2 == 0 }.sum()
