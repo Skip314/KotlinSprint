61a9e2fe0d1f88e9765952580e3e0fcfd0085375
Afana <afanasevlad@mail.ru>
2024-04-28 12:35:57 +0300

    KS

--- src/main/kotlin/lesson_21/lesson21_5.kt
+++ src/main/kotlin/lesson_21/lesson21_5.kt
@@ -2,12 +2,12 @@ package org.example.lesson_21
 
 fun main() {
 
-    val skill = mutableMapOf("sneak" to 5, "builder" to 5, "miner" to 5)
+    val skill = mutableMapOf("sneak" to 3, "builder" to 5, "miner" to 5)
     println(skill.maxCategory())
 }
 
 fun Map<String, Int>.maxCategory(): String? {
 
     val maxSkill = maxOf() { it.value }
-    return this.entries.shuffled().find { it.value == maxSkill }?.key
+    return this.maxByOrNull { it.value == maxSkill }?.key
 }
