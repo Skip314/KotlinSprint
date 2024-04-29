aae8dcc00604b7ae1a2d77ac87dec15fbc2de598
Afana <afanasevlad@mail.ru>
2024-04-29 15:03:14 +0300

    KS

--- src/main/kotlin/lesson_22/lesson22_4.kt
+++ src/main/kotlin/lesson_22/lesson22_4.kt
@@ -1,7 +1,36 @@
 package org.example.lesson_22
 
+class ViewModel(
+    var stat: MainScreenStat,
+) {
+    fun loadData(stat: Int): String {
+        return when (stat) {
+            1 -> "загрузка данных"
+            2 -> "наличие загруженных данных"
+            else -> "отсутствие данных"
+        }
+    }
+}
 
 data class MainScreenStat(
-    val data: String,
-    val isLoading: Boolean = false
-)
+    var data: String,
+    private var isLoading: Boolean = false,
+) {
+
+    fun changeIsLoad() {
+
+        this.copy()
+        isLoading = true
+    }
+}
+
+fun main() {
+
+    val stat = MainScreenStat("data")
+    val screen = ViewModel(stat)
+
+    println(screen.loadData(0))
+
+    stat.changeIsLoad()
+    println(screen.loadData(1))
+}
