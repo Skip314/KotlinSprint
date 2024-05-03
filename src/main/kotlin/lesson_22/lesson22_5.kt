8c7fc29846d0baa82d7c82d6611a31bf70fe39de
Afana <afanasevlad@mail.ru>
2024-05-03 10:50:46 +0300

    KS

--- src/main/kotlin/lesson_22/lesson22_5.kt
+++ src/main/kotlin/lesson_22/lesson22_5.kt
@@ -5,22 +5,15 @@ data class GalacticGuide(
     val description: String,
     val date: String,
     val distance: Float,
-) {
-    fun component_1() = name
-    fun component_2() = description
-    fun component_3() = date
-    fun component_4() = distance
-
-
-}
+)
 
 fun main() {
 
     val guide = GalacticGuide("Альфа Центавра", "Ближайшая к Земле звёздная система", "1 января 2024", 4.37f)
 
-    println("Название: ${guide.component_1()}")
-    println("Описание: ${guide.component_2()}")
-    println("Дата: ${guide.component_3()}")
-    println("Дистанция: ${guide.component_4()}")
+    println("Название: ${guide.component1()}")
+    println("Описание: ${guide.component2()}")
+    println("Дата: ${guide.component3()}")
+    println("Дистанция: ${guide.component4()}")
 }
 
